5e584e5623e12746b11ec5e9e0d65791b7658b5f
robert <robertquillaslopez@gmail.com>
2025-07-26 00:38:47 -0500

    Init Ansible estructure

--- terraform/enviroments/dev/main.tf
+++ terraform/enviroments/dev/main.tf
@@ -20,22 +20,22 @@ module "azurerm_resource_group" {
   location            = var.location
 }
 
-# module "network" {
-#   source              = "../../modules/network"
-#   resource_group_name = module.azurerm_resource_group.resource_group_name
-#   location            = module.azurerm_resource_group.resource_group_location
-# }
+module "network" {
+  source              = "../../modules/network"
+  resource_group_name = module.azurerm_resource_group.resource_group_name
+  location            = module.azurerm_resource_group.resource_group_location
+}
 
-# module "vm" {
-#   source               = "../../modules/virtual_machine"
-#   vm_name              = "vm-cp2"
-#   location             = var.location
-#   resource_group_name  = var.resource_group_name
-#   vm_size              = "Standard_B1s"
-#   admin_username       = var.admin_username
-#   public_key_path      = var.public_key_path
-#   network_interface_id = module.network.network_interface_id
-# }
+module "vm" {
+  source               = "../../modules/virtual_machine"
+  vm_name              = "vm-cp2"
+  location             = var.location
+  resource_group_name  = var.resource_group_name
+  vm_size              = "Standard_B1s"
+  admin_username       = var.admin_username
+  public_key_path      = var.public_key_path
+  network_interface_id = module.network.network_interface_id
+}
 
 module "acr" {
   source              = "../../modules/acr"
@@ -46,20 +46,19 @@ module "acr" {
   admin_enabled       = true
 }
 
-
-module "aks" {
-  source              = "../../modules/aks"
-  aks_name            = "aks-cp2"
-  resource_group_name = var.resource_group_name
-  location            = var.location
-  dns_prefix          = "dns-cp2"
-  acr_id              = module.acr.acr_id
-  node_count          = 1
-  vm_size             = "Standard_B1s"
-  tags = {
-    namespace = "dev"
-  }
-}
+#module "aks" {
+#  source              = "../../modules/aks"
+#  aks_name            = "aks-cp2"
+#  resource_group_name = var.resource_group_name
+#  location            = var.location
+#  dns_prefix          = "dns-cp2"
+#  acr_id              = module.acr.acr_id
+#  node_count          = 1
+#  vm_size             = "Standard_B1s"
+#  tags = {
+#    namespace = "dev"
+#  }
+#}
 
 
 #resource "azurerm_role_assignment" "aks_acr_pull" {
